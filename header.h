6378879c65a1f4edc91c6bb706ced1701d81edff
Loirito <joaofranciscosantosferreira@gmail.com>
2019-11-09 02:01:15 +0000

    Added shared memory creation and cleanup, also added initial structs for flights, stats, and flight lists

---
+++ header.h
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/msg.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <math.h>
+#include <stdbool.h>
+
+#define ARRIVAL_T 8
+#define DEPARTURE_T 8
+#define MAX_ARRIVAL 5
+//#define MAX_DEPARTURE 8
+
+typedef struct arrivals {
+	bool can_arrive;
+	bool holding;
+	bool leave;
+	int added_time;
+	int eta;
+	int init;
+	int fuel;
+	int flight_id;
+	int priority;
+} arrival_flight;
+arrival_flight *arrivals_array;
+
+typedef struct departures {
+	bool can_depart;
+	int init;
+	int departure_time;
+	int flight_id;
+	int priority;
+} departure_flight;
+departure_flight *departures_array;
+
+typedef struct list_arrivals *arrival_list;
+typedef struct list_arrivals {
+	arrival_flight flight;
+	int n_slot;
+	arrival_list *next;
+} list_arrivals;
+
+typedef struct list_departures *departures_list;
+typedef struct list_departures {
+	departure_flight flight;
+	int n_slot;
+	departures_list *next;
+} list_departures;
+
+typedef struct shared_memory {
+	list_departures departures_list;
+	list_arrivals arrival_list;
+	int total_flights;
+	int total_landed_flights;
+	double avg_arrival_time;	// this value starts counting after the eta of the flight (so the standard should be the arrival motion T units of time)
+	int total_departures;
+	double avg_departure_time;
+	double avg_holdings_per_landing;
+	double avg_holdings_per_urgent_landing;
+	int total_deviated_flights;
+	int total_rejections;
+} shmem;
+shmem *shm;
--- projeto.c
+++ projeto.c
@@ -1,31 +1,76 @@
-%Projeto SO
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
+//Projeto SO
+//gcc -Wall -lpthread -D_REENTRANT mutex_between_procs_no.c -o mbp_n
 
+#include "header.h"
+
+shmem *shm;
 
 pid_t id;
 int shmid;
 
-void initializer() {
+void create_shm() {
 
+	if((shmid = shmget(IPC_PRIVATE, sizeof(shmem), IPC_CREAT | 0777)) == -1) {
+		perror("Shared memory creation unsuccessful.\n");
+		exit(1);
+	}
+	shm = (shmem*) shmat(shmid, NULL, 0);
+
+	printf("--------------SHARED MEMORY--------------\n");
+	printf("Shared memory succesfully created at mem address %p\n", shm);
+	printf("Shared memory id is %d\n", shmid);
+	printf("Initializing shared memory values now.\n");
+
+	//shm->departures_list = departures_list;
+	//shm->arrival_list = arrival_list;
+	shm->total_flights = 0;
+	shm->total_landed_flights = 0;
+	shm->avg_arrival_time = 0.0;
+	shm->total_departures = 0;
+	shm->avg_departure_time = 0.0;
+	shm->avg_holdings_per_landing = 0.0;
+	shm->avg_holdings_per_urgent_landing = 0.0;
+	shm->total_deviated_flights = 0;
+	shm->total_rejections = 0;
+
+	printf("Shared memory values successfully initialized.\n");
+
+	return;
+}
+
+void initializer() {
+	create_shm();
+
+	return;
+}
+
+void cleanup() {
+	shmdt(shm);
+	shmctl(shmid, IPC_RMID, NULL);
+	
+	return;
 }
 
 int main(void) {
+
+	initializer();
 
 	id = fork();
 
 	if(id == 0) {
 		//control tower initializer
 		printf("Control Tower created with PID %ld.\n", (long)getpid());
+		return 0;
 	}
 
 	else if(id == -1) {
 		perror("Error forking.\n");
+		return 1;
 	}
 
 	else {
 		printf("Sim manager here.\n");
+		return 0;
 	}
 
 	return 0;
